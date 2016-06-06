4c1bfc52c2c90c729c5fa592806f6d1d7a82b202
Florent Lévigne <florent.levigne@mailoo.org>
2016-06-06 15:21:19 +0200

    dotfiles

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Inspired by https://github.com/hcartiaux/dotfiles/blob/master/install.sh
+
+set -x # Debug
+
+DOTFILES=~/.dotfiles
+
+[[ ! -d ~/.dotfiles ]] && git clone git@github.com:Misaflo/dotfiles.git $DOTFILES
+[[   -d ~/.dotfiles ]] && ( cd $DOTFILES ; git pull )
+
+
+#
+# vim
+#
+mkdir ~/.vim
+cd ~/.vim
+mkdir bundle
+[[ ! -d bundle/vundle ]] && git clone https://github.com/gmarik/vundle.git bundle/Vundle.vim
+[[   -d bundle/vundle ]] && ( cd bundle/vundle ; git pull )
+
+ln -sf $DOTFILES/vim/vimrc ~/.vimrc
+[[ ! -h ~/.vim/header ]] && ln -sf $DOTFILES/vim/header ~/.vim/header
+[[ ! -h ~/.vim/syntax ]] && ln -sf $DOTFILES/vim/syntax ~/.vim/syntax
+
+
+#
+# zsh
+#
+mkdir ~/.zsh
+cd ~/.zsh
+[[ ! -d ~/.zsh/zsh-syntax-highlighting ]] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+[[   -d ~/.zsh/zsh-syntax-highlighting ]] && (cd ~/.zsh/zsh-syntax-highlighting ; git pull )
+
+ln -sf $DOTFILES/zsh/zshrc ~/.zshrc
+
+
+#
+# git
+#
+ln -sf $DOTFILES/git/gitconfig ~/.gitconfig
---
+++ vim/syntax/todo.vim
@@ -0,0 +1,27 @@
+" Vim syntax file
+" Language: mytodo
+" Maintainer: Jean-Baptiste Hétier
+" Latest Revision: 04/08/2011
+" URL : http://www.think-underground.com/post/2012/02/16/G%C3%A9rer-sa-liste-de-todos-dans-Vim
+
+if exists("b:current_syntax")
+  finish
+endif
+
+syn match todoTitle '^\S\+.*$'
+syn match todoPeople '@\S\+'
+syn match todoPeople '@\S\+\s\u\S*'
+"syn match todoProject '\s+\S\+' " original
+syn match todoProject '^ \s+\+.*$'
+syn match todoImportant '^\s*!\s.*$' contains=todoProject,todoPeople,todoDate
+syn match todoList '^\s*\*\s'
+syn match todoDate /\d\{4\}\/\d\{2\}\/\d\{2\}/
+
+hi todoTitle term=bold ctermfg=DarkCyan guifg=DarkCyan gui=bold
+hi todoPeople ctermfg=DarkGreen guifg=DarkGreen
+hi todoProject term=bold ctermfg=DarkYellow guifg=DarkYellow gui=bold
+hi todoImportant term=bold ctermfg=Red guifg=Red gui=bold
+hi todoList ctermfg=DarkMagenta guifg=DarkMagenta
+hi todoDate term=underline gui=underline
+
+let b:current_syntax = "mytodo"
