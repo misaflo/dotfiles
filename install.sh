489f4cfe1b70f69bf9647363b097e4757444d906
Florent Lévigne <florent.levigne@mailoo.org>
2019-03-28 09:43:29 +0100

    vim: add plugin vim-markdown-preview

--- install.sh
+++ install.sh
@@ -12,6 +12,10 @@ fi
 if [[ ! -L ~/.config/nvim ]]; then
   ln -sf $DOTFILES/vim ~/.config/nvim
 fi
+if [[ ! -f ~/.dotfiles/vim/github-pandoc.css ]]; then
+  wget https://gist.githubusercontent.com/dashed/6714393/raw/ae966d9d0806eb1e24462d88082a0264438adc50/github-pandoc.css -O ~/.dotfiles/vim/github-pandoc.css
+  echo "html,body {max-width: 65em;}" >> ~/.dotfiles/vim/github-pandoc.css
+fi
 
 
 #
@@ -19,7 +23,7 @@ fi
 #
 if [[ ! -d ~/.zsh/zsh-syntax-highlighting ]]; then
   mkdir ~/.zsh
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git .zsh/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 fi
 ln -sf $DOTFILES/zsh/zshrc ~/.zshrc
 
