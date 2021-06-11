e2cfe8b18c4df34a2cb957adee83b458fce49ce1
Florent Lévigne <florent.levigne@mailoo.org>
2021-06-11 10:44:02 +0200

    Add fontconfig

--- install.sh
+++ install.sh
@@ -2,69 +2,44 @@
 
 DOTFILES="$HOME/.dotfiles"
 
-#
 # neovim
-#
-
-# vim-plug
 vimplug_dir="$HOME/.local/share/nvim/site/autoload"
 if [[ ! -f "$vimplug_dir/plug.vim" ]]; then
   mkdir -p "$vimplug_dir"
   wget https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -O "$vimplug_dir/plug.vim"
 fi
-
 [[ ! -d ~/.config ]] && mkdir ~/.config
 [[ ! -L ~/.config/nvim ]] && ln -sf "$DOTFILES/nvim" ~/.config/nvim
 
-
-#
 # zsh
-#
 if [[ ! -d ~/.zsh/zsh-syntax-highlighting ]]; then
   mkdir ~/.zsh
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 fi
 ln -sf "$DOTFILES/zsh/zshrc" ~/.zshrc
 
-
-#
 # starship
-#
 ln -sf "$DOTFILES/starship/starship.toml" ~/.config/starship.toml
 
-
-#
 # git
-#
 ln -sf "$DOTFILES/git/gitconfig" ~/.gitconfig
 
-
-#
 # wofi
-#
 [[ ! -L ~/.config/wofi ]] && ln -sf "$DOTFILES/wofi" ~/.config/wofi
 
-
-#
 # alacritty
-#
 [[ ! -L ~/.config/alacritty ]] && ln -sf "$DOTFILES/alacritty" ~/.config/alacritty
 
-
-#
 # bat
-#
 [[ ! -d ~/.config/bat ]] && mkdir ~/.config/bat
 [[ ! -L ~/.config/bat/config ]] && ln -sf "$DOTFILES/bat/config" ~/.config/bat/config
 
-
-#
 # profanity
-#
 [[ -d ~/.config/profanity ]] && ln -sf "$DOTFILES/profanity/profrc" ~/.config/profanity/profrc
 
-
-#
 # mailcap
-#
 ln -sf "$DOTFILES/mailcap" ~/.mailcap
+
+# fontconfig
+[[ ! -d ~/.config/fontconfig ]] && mkdir ~/.config/fontconfig
+[[ ! -L ~/.config/fontconfig/fonts.conf ]] && ln -sf "$DOTFILES/fontconfig/fonts.conf" ~/.config/fontconfig/fonts.conf
