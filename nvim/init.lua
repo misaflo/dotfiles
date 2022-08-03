94eaf0d971779bb83307bef345cb8fd811c49437
Florent Lévigne <florent.levigne@mailoo.org>
2022-08-03 13:41:23 +0200

    nvim: add project.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -278,6 +278,17 @@ require('packer').startup(function()
   -- Toggles between hybrid and absolute line numbers automatically
   use 'jeffkreeftmeijer/vim-numbertoggle'
 
+  -- Autodect and cd to project directory
+  use {
+    'ahmedkhalf/project.nvim',
+    config = function()
+      require('project_nvim').setup {
+        detection_methods = { 'pattern' },
+        patterns = { '.git' },
+      }
+    end
+  }
+
   ----------------------------------------
   --------------- Snippets ---------------
   ----------------------------------------
