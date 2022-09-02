559d9a53e34366fb33b9c3c8eda441fdbd474a62
Florent Lévigne <florent.levigne@mailoo.org>
2022-09-02 09:15:07 +0200

    nvim: remove autopairs plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -241,18 +241,6 @@ require('packer').startup(function()
     end,
   }
 
-  -- Autopairs
-  use {
-    'windwp/nvim-autopairs',
-    config = function()
-      require('nvim-autopairs').setup()
-
-      local Rule = require('nvim-autopairs.rule')
-      local npairs = require('nvim-autopairs')
-      npairs.add_rule(Rule('« ',' »'))
-    end,
-  }
-
   -- Add/change/delete surrounding delimiter pairs
   use {
     'kylechui/nvim-surround',
