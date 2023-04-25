b26ebd35c8ff616065aaff50f81308ebf84fcf7d
Florent Lévigne <florent.levigne@mailoo.org>
2023-04-25 08:58:45 +0200

    nvim: add some LSP plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -426,4 +426,97 @@ require('lazy').setup({
 
   -- Syntax for highlighting YARD documentation
   'noprompt/vim-yardoc',
+
+  ----------------------------------------
+  ----------------- LSP ------------------
+  ----------------------------------------
+
+  -- Install and manage LSP servers, DAP servers, linters, and formatters
+  {
+    'williamboman/mason.nvim',
+    build = ":MasonUpdate",
+    config = true,
+  },
+
+  -- Makes it easier to use lspconfig with mason.nvim
+  {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require('mason-lspconfig').setup()
+      -- Automatic server setup
+      require('mason-lspconfig').setup_handlers {
+        function (server_name)
+          require('lspconfig')[server_name].setup {}
+        end,
+      }
+    end,
+  },
+
+  -- Quickstart configs for Neovim LSP
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      -- Setup language servers.
+      local lspconfig = require('lspconfig')
+
+      -- Global mappings.
+      -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+      vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+      vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+      -- Use LspAttach autocommand to only map the following keys
+      -- after the language server attaches to the current buffer
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+          -- Buffer local mappings.
+          -- See `:help vim.lsp.*` for documentation on any of the below functions
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+          vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+          vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+          vim.keymap.set('n', '<space>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, opts)
+          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', '<space>f', function()
+            vim.lsp.buf.format { async = true }
+          end, opts)
+        end,
+      })
+    end,
+  },
+
+  -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more
+  {
+    'jose-elias-alvarez/null-ls.nvim',
+    dependencies = 'nvim-lua/plenary.nvim',
+    config = function()
+      local null_ls = require('null-ls')
+
+      null_ls.setup {
+        sources = {
+          -- Shell
+          null_ls.builtins.diagnostics.shellcheck,
+          -- YAML
+          null_ls.builtins.diagnostics.yamllint,
+          -- Puppet
+          null_ls.builtins.diagnostics.puppet_lint,
+          null_ls.builtins.formatting.puppet_lint,
+        },
+      }
+    end,
+  },
 }, { diff = { cmd = 'terminal_git' }})
