b90df697853e3e2e83bd5087577bc2745467000c
Florent Lévigne <florent.levigne@mailoo.org>
2023-05-14 08:52:27 +0200

    nvim: add nvim-cmp

--- nvim/init.lua
+++ nvim/init.lua
@@ -533,4 +533,47 @@ require('lazy').setup({
       }
     end,
   },
+
+  ----------------------------------------
+  -------------- Completion --------------
+  ----------------------------------------
+
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'dcampos/cmp-snippy',
+    },
+    event = 'InsertEnter',
+    config = function()
+      local cmp = require('cmp')
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require('snippy').expand_snippet(args.body)
+          end,
+        },
+        completion = {
+          autocomplete = false,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-x><C-o>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'snippy' },
+        }, {
+            { name = 'buffer' },
+          })
+      })
+    end,
+  }
 }, { diff = { cmd = 'terminal_git' }})
