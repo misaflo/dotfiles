ba4e8939cbc046ddfbc0306575bf9417ef7f207a
Florent Lévigne <florent.levigne@mailoo.org>
2023-05-21 13:09:55 +0200

    nvim: add dependencies for aerial

--- nvim/init.lua
+++ nvim/init.lua
@@ -333,6 +333,10 @@ require('lazy').setup({
   -- Outline window for quick navigation
   {
     'stevearc/aerial.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons'
+    },
     keys = { { '<F9>', ':AerialToggle<CR>' } },
     config = true,
   },
