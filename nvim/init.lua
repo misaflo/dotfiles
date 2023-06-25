ca022f399083300345f09749aba5b0e12d9443b7
Florent Lévigne <florent.levigne@mailoo.org>
2023-06-25 11:38:33 +0200

    nvim: update gitsigns config

--- nvim/init.lua
+++ nvim/init.lua
@@ -357,13 +357,28 @@ require('lazy').setup({
   -- Git integration: signs, hunk actions, blame, etc.
   {
     'lewis6991/gitsigns.nvim',
-    lazy = false,
-    keys = {
-      { '<leader>gb', ":lua require('gitsigns').toggle_current_line_blame()<CR>" },
-      { '<leader>gd', ":lua require('gitsigns').diffthis()<CR>" },
-      { '<leader>gm', ":lua require('gitsigns').blame_line{full=true}<CR>" },
-    },
-    config = true,
+    opts = {
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then return ']c' end
+          vim.schedule(function() gs.next_hunk() end)
+          return '<Ignore>'
+        end, {expr=true})
+
+        map('n', '[c', function()
+          if vim.wo.diff then return '[c' end
+          vim.schedule(function() gs.prev_hunk() end)
+          return '<Ignore>'
+        end, {expr=true})
+
+        -- Actions
+        map('n', '<leader>gb', gs.toggle_current_line_blame)
+        map('n', '<leader>gd', gs.diffthis)
+      end
+    }
   },
 
   -- Magit clone: stage, commit, pull, push
