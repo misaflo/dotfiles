6c93fbd91e1ee8ed2a5bcbd0d93c7b2fc17398a8
Florent Lévigne <florent.levigne@mailoo.org>
2023-10-04 11:05:45 +0200

    nvim: remove grammalecte

--- nvim/init.lua
+++ nvim/init.lua
@@ -323,31 +323,6 @@ require('lazy').setup({
     },
   },
 
-  -- French grammar checker
-  {
-    'dpelle/vim-Grammalecte',
-    keys = {
-      { '<leader>gc', ':GrammalecteCheck<CR>' },
-      { '<leader>gl', ':GrammalecteClear<CR>' }
-    },
-    config = function()
-      vim.g.grammalecte_cli_py = '~/.dotfiles/nvim/grammalecte/grammalecte-cli.py'
-      vim.g.grammalecte_disable_rules = 'typo_tiret_début_ligne typo_tiret_incise2' ..
-      ' apostrophe_typographique apostrophe_typographique_après_t' ..
-      ' espaces_début_ligne espaces_milieu_ligne espaces_fin_de_ligne' ..
-      ' esp_début_ligne esp_milieu_ligne esp_fin_ligne esp_mélangés2' ..
-      ' typo_points_suspension1 typo_tiret_incise' ..
-      ' nbsp_avant_double_ponctuation nbsp_avant_deux_points' ..
-      ' nbsp_après_chevrons_ouvrants nbsp_avant_chevrons_fermants1' ..
-      ' unit_nbsp_avant_unités1 unit_nbsp_avant_unités2' ..
-      ' unit_nbsp_avant_unités3 typo_guillemets_typographiques_doubles_ouvrants' ..
-      ' typo_guillemets_typographiques_doubles_fermants' ..
-      ' typo_tiret_incise1'
-      vim.cmd 'highlight link GrammalecteGrammarError spellCap'
-      vim.cmd 'highlight link GrammalecteSpellingError spellBad'
-    end,
-  },
-
   -- Alignment
   {
     'junegunn/vim-easy-align',
