5177d949fc7a7a81be4e87b2e0a81215212f7510
Florent Lévigne <florent.levigne@mailoo.org>
2024-01-10 10:33:07 +0100

    nvim: table-mode, disable syntax

--- nvim/lua/plugins/table-mode.lua
+++ nvim/lua/plugins/table-mode.lua
@@ -4,5 +4,6 @@ return {
   keys = { { '<leader>tm', ':TableModeToggle<CR>' } },
   config = function()
     vim.g.table_mode_corner = '|' -- markdown-compatible tables
+    vim.g.table_mode_syntax = 0 -- improve performance
   end,
 }
