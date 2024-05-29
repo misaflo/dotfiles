97f802e64adafce535acb6ed0c688529701397c2
Florent Lévigne <florent.levigne@mailoo.org>
2024-05-29 16:26:40 +0200

    nvim: colorizer, exclude for mail

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -2,6 +2,6 @@
 return {
   'NvChad/nvim-colorizer.lua',
   opts = {
-    filetypes = { '*', '!lazy', '!gitcommit', '!NeogitCommitMessage', '!lazy' },
+    filetypes = { '*', '!lazy', '!gitcommit', '!NeogitCommitMessage', '!lazy', '!mail' },
   },
 }
