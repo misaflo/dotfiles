a42503d00d2280d7245e31fb7e327e249928f0c4
Florent Lévigne <florent.levigne@mailoo.org>
2024-06-19 09:10:23 +0200

    nvim: update conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,7 +6,7 @@ return {
     {
       '<space>f',
       function()
-        require('conform').format({ async = true, lsp_fallback = true })
+        require('conform').format({ async = true, lsp_format = 'fallback' })
       end,
       mode = '',
       desc = 'Format buffer',
