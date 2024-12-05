a779ef2995d4a4abdd31aa447189cf41ec3c9b43
Florent Lévigne <florent.levigne@mailoo.org>
2024-12-05 10:45:15 +0100

    nvim: update gitsigns functions

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -3,34 +3,32 @@ return {
   'lewis6991/gitsigns.nvim',
   opts = {
     on_attach = function(bufnr)
-      local gs = package.loaded.gitsigns
+      local gitsigns = require('gitsigns')
 
       -- Navigation
       vim.keymap.set('n', ']c', function()
         if vim.wo.diff then
-          return ']c'
+          vim.cmd.normal({ ']c', bang = true })
+        else
+          gitsigns.nav_hunk('next')
         end
-        vim.schedule(function()
-          gs.next_hunk()
-        end)
-        return '<Ignore>'
-      end, { expr = true })
+      end)
 
       vim.keymap.set('n', '[c', function()
         if vim.wo.diff then
-          return '[c'
+          vim.cmd.normal({ '[c', bang = true })
+        else
+          gitsigns.nav_hunk('prev')
         end
-        vim.schedule(function()
-          gs.prev_hunk()
-        end)
-        return '<Ignore>'
-      end, { expr = true })
+      end)
 
       -- Actions
-      vim.keymap.set('n', '<leader>gb', gs.toggle_current_line_blame)
-      vim.keymap.set('n', '<leader>gd', gs.diffthis)
-      vim.keymap.set('n', '<leader>gr', gs.reset_hunk)
-      vim.keymap.set('v', '<leader>gr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+      vim.keymap.set('n', '<leader>gb', gitsigns.toggle_current_line_blame)
+      vim.keymap.set('n', '<leader>gd', gitsigns.diffthis)
+      vim.keymap.set('n', '<leader>gr', gitsigns.reset_hunk)
+      vim.keymap.set('v', '<leader>gr', function()
+        gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+      end)
     end,
   },
 }
