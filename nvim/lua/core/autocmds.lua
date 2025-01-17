9b0382e1fa4b38b25f494775a1a5158b38a37642
Florent Lévigne <florent.levigne@mailoo.org>
2025-01-17 10:09:39 +0100

    nvim: fix autocmd for help ft

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -19,12 +19,14 @@ autocmd('TextYankPost', {
 })
 
 autocmd('FileType', {
-  pattern = 'help',
+autocmd('BufWinEnter', {
+  pattern = '*.txt',
   desc = 'Open help in vertical split',
   callback = function()
-    if vim.api.nvim_win_get_width(0) > 180 then
+    if vim.bo.filetype == 'help' and vim.api.nvim_win_get_width(0) > 180 then
       vim.cmd.wincmd('L')
     end
   end,
   group = augroup('help'),
 })
+
