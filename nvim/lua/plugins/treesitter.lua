08f4495b55eb2bceda91b97932ae8ed27458f624
Florent Lévigne <florent.levigne@mailoo.org>
2025-01-19 11:43:51 +0100

    nvim: treesitter, set auto_install

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
   build = ':TSUpdate',
   config = function()
     require('nvim-treesitter.configs').setup({
+      auto_install = true,
       highlight = {
         enable = true,
       },
