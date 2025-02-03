a42aed9c3f23ac026fdefefcc5ca9ff575ebe146
Florent Lévigne <florent.levigne@mailoo.org>
2025-02-03 17:10:20 +0100

    nvim: csvview, add keymaps

--- nvim/lua/plugins/csvview.lua
+++ nvim/lua/plugins/csvview.lua
@@ -1,5 +1,15 @@
+-- CSV file editing
 return {
   'hat0uma/csvview.nvim',
   ft = 'csv',
-  config = true,
+  opts = {
+    keymaps = {
+      -- Text objects for selecting fields
+      textobject_field_inner = { 'if', mode = { 'o', 'x' } },
+      textobject_field_outer = { 'af', mode = { 'o', 'x' } },
+      -- Use <Tab> and <S-Tab> to move horizontally between fields.
+      jump_next_field_end = { '<Tab>', mode = { 'n', 'v' } },
+      jump_prev_field_end = { '<S-Tab>', mode = { 'n', 'v' } },
+    },
+  },
 }
