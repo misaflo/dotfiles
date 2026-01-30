7f2c554e913520479fd0ede7004fe8ccc8ef8ab4
Florent Lévigne <florent.levigne@mailoo.org>
2026-01-30 17:25:56 +0100

    nvim: conform, always use lsp_format

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -6,7 +6,7 @@ return {
     {
       '<space>f',
       function()
-        require('conform').format({ async = true, lsp_format = 'fallback' })
+        require('conform').format({ async = true, lsp_format = 'first' })
       end,
       mode = { 'n', 'v' },
       desc = 'Format buffer',
