dd9d4a501f11e604e4e25baeef6f5769de411abd
Florent Lévigne <florent.levigne@mailoo.org>
2026-03-30 17:55:53 +0200

    nvim: highlight-colors, ignore nvim-pack ft

--- nvim/plugin/highlight-colors.lua
+++ nvim/plugin/highlight-colors.lua
@@ -1,4 +1,8 @@
 -- Color name highlighter
 vim.pack.add({ 'https://github.com/brenoprata10/nvim-highlight-colors' })
 
-require('nvim-highlight-colors').setup()
+require('nvim-highlight-colors').setup({
+  exclude_filetypes = {
+    'nvim-pack',
+  },
+})
