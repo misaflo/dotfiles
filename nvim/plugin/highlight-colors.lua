b3117430c99df5a48de96f2715b069a92fc4895b
Florent Lévigne <florent.levigne@mailoo.org>
2026-04-04 16:43:33 +0200

    nvim: nvim-highlight-colors only for some ft

--- nvim/plugin/highlight-colors.lua
+++ nvim/plugin/highlight-colors.lua
@@ -1,8 +1,17 @@
 -- Color name highlighter
 vim.pack.add({ 'https://github.com/brenoprata10/nvim-highlight-colors' })
 
-require('nvim-highlight-colors').setup({
-  exclude_filetypes = {
-    'nvim-pack',
+local hlc = require('nvim-highlight-colors')
+hlc.setup()
+hlc.turnOff()
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {
+    'css',
+    'swayconfig',
   },
+  callback = function(event)
+    hlc.turnOn()
+  end,
+  group = vim.api.nvim_create_augroup('misaflo_hlc', { clear = true }),
 })
