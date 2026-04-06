090411ab797b0862d6b54e27d192465fb144fe52
Florent Lévigne <florent.levigne@mailoo.org>
2026-04-06 18:23:03 +0200

    nvim: manually set root directory

---
+++ nvim/lua/misc.lua
@@ -0,0 +1,2 @@
+-- Set root directory
+vim.cmd('cd ' .. vim.fs.root(0, '.git'))
--- nvim/plugin/rooter.lua
+++
@@ -1,4 +0,0 @@
--- Changes working directory to project root
-vim.pack.add({ 'https://github.com/notjedi/nvim-rooter.lua' })
-
-require('nvim-rooter').setup()
