917f366673516018164b4f2691364e74d61a4f96
Florent Lévigne <florent.levigne@mailoo.org>
2026-04-06 20:29:21 +0200

    nvim: fix setting root directory

--- nvim/lua/misc.lua
+++ nvim/lua/misc.lua
@@ -1,5 +1,6 @@
 -- Set root directory
-vim.cmd('cd ' .. vim.fs.root(0, '.git'))
+local directory = vim.fs.root(0, '.git') or '.'
+vim.cmd.cd(directory)
 
 -- Update plugins
 vim.api.nvim_create_user_command('PackUpdate', function()
