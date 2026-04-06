80e4e88dd639072cab4d725159e69a4203baa306
Florent Lévigne <florent.levigne@mailoo.org>
2026-04-06 18:27:28 +0200

    nvim: move PackUpdate to misc.lua

--- nvim/lua/misc.lua
+++ nvim/lua/misc.lua
@@ -1,2 +1,7 @@
 -- Set root directory
 vim.cmd('cd ' .. vim.fs.root(0, '.git'))
+
+-- Update plugins
+vim.api.nvim_create_user_command('PackUpdate', function()
+  vim.pack.update()
+end, { desc = 'Update plugins' })
