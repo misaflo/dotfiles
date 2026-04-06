8cdc2c848b4020f516b57545324c673c3b1c3c1f
Florent Lévigne <florent.levigne@mailoo.org>
2026-04-06 11:54:48 +0200

    nvim: lsp, add lua_ls

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -50,5 +50,51 @@ vim.lsp.config('ltex_plus', {
 -- solargraph (Ruby)
 vim.lsp.enable('solargraph')
 
+-- lua-language-server
+vim.lsp.enable('lua_ls')
+-- See https://github.com/neovim/nvim-lspconfig/blob/master/lsp/lua_ls.lua
+-- for neovim specific config
+vim.lsp.config('lua_ls', {
+  on_init = function(client)
+    -- Disable lua_ls formater (EmmyLuaCodeStyle), use stylua with conform
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentFormattingRangeProvider = false
+
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath('config')
+        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most
+        -- likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Tell the language server how to find Lua modules same way as Neovim
+        -- (see `:h lua-module-load`)
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
+        },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+        },
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
+  },
+})
+
 -- Install and manage LSP servers, DAP servers, linters, and formatters
 require('mason').setup()
